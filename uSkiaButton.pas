2ca4d99dac4505a8696a0c003452926ef22104bf
Lara Miriam Tamy del Fabro <133981326+LaMitaOne@users.noreply.github.com>
2026-08-05 16:12:24 +0200

    Add files via upload

--- uSkiaButton.pas
+++ uSkiaButton.pas
@@ -1,110 +1,91 @@
 {*******************************************************************************
 TSkiaButton
-
 Modern, animated button control built with Skia4Delphi.
 Designed for smooth, responsive hover interactions with visual polish.
 Key Features:
-
 Ripple hover: growing radial white pulse from center
 Liquid hover: animated flowing dashed border with phase shift
 Breathing pulse: subtle sine-based scale variation + shine overlay
 Tilt hover: mouse-position based parallax shift and squash
 Customizable: corner radius, effect types, colors, animation speed
-
 *******************************************************************************}
-{ TSkiaButton v0.1 alpha                                                       }
+{ TSkiaButton v0.2 alpha                                                       }
 { by Lara Miriam Tamy Reschke                                                   }
 {                                                                              }
 {------------------------------------------------------------------------------}
 {
 ----Latest Changes
-
+   v 0.2:
+    - Compatibility fix for delphi 10.2 (thanks to limelect)
    v 0.1:
-    -first release
-
+    - first release
 }
 unit uSkiaButton;
 
 interface
-
 uses
   System.SysUtils, System.Types, System.Classes, System.Math, System.UITypes,
   FMX.Types, FMX.Controls, FMX.StdCtrls,
   FMX.Skia, System.Skia;
-
 type
   TButtonStyle = (bsFlat, bsNeon, bsRetro);
   THoverEffect = (heGlow, heNone, heScaleUp, heRipple, heLiquid, heTilt, heSpot);
   TTransitionType = (ttFade, ttImplode, tsExplode);
-
   TSkiaButton = class(TSkCustomControl)
   private
     FTimer: TTimer;
     FBackBuffer: ISkImage;
-
     FText: string;
     FButtonStyle: TButtonStyle;
     FHoverEffect: THoverEffect;
     FShowTransition: TTransitionType;
     FHideTransition: TTransitionType;
-
     // Color Properties
     FColor: TAlphaColor;
     FHoverColor: TAlphaColor;
     FPressedColor: TAlphaColor;
     FTextColor: TAlphaColor;
-
     // New Border Properties
     FBorderColor: TAlphaColor;
     FBorderWidth: Single;
     FRoundCorners: Single;
     FShowBorder: Boolean;
-
     FIsHovered: Boolean;
     FIsPressed: Boolean;
     FMousePos: TPointF;
-
     FAnimationState: (asIdle, asShowing, asHiding);
     FAnimProgress: Single;
     FTime: Double;
-
     FOnClick: TNotifyEvent;
     FOnMouseDown: TMouseEvent;
     FOnMouseUp: TMouseEvent;
     FOnMouseEnter: TNotifyEvent;
     FOnMouseLeave: TNotifyEvent;
-
     procedure SetText(const Value: string);
     procedure SetButtonStyle(const Value: TButtonStyle);
     procedure SetHovered(const Value: Boolean);
     procedure SetPressed(const Value: Boolean);
-
     // Setters for colors
     procedure SetColor(const Value: TAlphaColor);
     procedure SetHoverColor(const Value: TAlphaColor);
     procedure SetPressedColor(const Value: TAlphaColor);
     procedure SetTextColor(const Value: TAlphaColor);
-
     // Setters for Border
     procedure SetBorderColor(const Value: TAlphaColor);
     procedure SetBorderWidth(const Value: Single);
     procedure SetRoundCorners(const Value: Single);
     procedure SetShowBorder(const Value: Boolean);
-
     procedure OnTimer(Sender: TObject);
     procedure UpdateState;
   protected
     procedure InvalidateBuffer;
     procedure Draw(const ACanvas: ISkCanvas; const ADest: TRectF; const AOpacity: Single); override;
-
     procedure MouseMove(Shift: TShiftState; X, Y: Single); override;
     procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
     procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
     procedure DoMouseLeave; override;
-
     procedure RenderToBuffer(const AWidth, AHeight: Integer);
     procedure DrawCenteredText(const ACanvas: ISkCanvas; const AText: string; const ADest: TRectF; const AFont: ISkFont; const APaint: ISkPaint);
-
     // Styles
     procedure DrawFlatStyle(const ACanvas: ISkCanvas; const ARect: TRectF; const AOpacity: Single);
     procedure DrawNeonStyle(const ACanvas: ISkCanvas; const ARect: TRectF; const AOpacity: Single);
@@ -112,35 +93,29 @@ type
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
-
     procedure ShowAnimated;
     procedure HideAnimated;
-
     property Text: string read FText write SetText;
     property ButtonStyle: TButtonStyle read FButtonStyle write SetButtonStyle;
   published
     property HideTransition: TTransitionType read FHideTransition write FHideTransition default ttImplode;
     property ShowTransition: TTransitionType read FShowTransition write FShowTransition default ttFade;
     property HoverEffect: THoverEffect read FHoverEffect write FHoverEffect default heGlow;
-
     // Color Properties
     property Color: TAlphaColor read FColor write SetColor default $FF3B82F6;
     property HoverColor: TAlphaColor read FHoverColor write SetHoverColor default $FF4B8BF6;
     property PressedColor: TAlphaColor read FPressedColor write SetPressedColor default $FF2A6ACB;
     property TextColor: TAlphaColor read FTextColor write SetTextColor default TAlphaColors.White;
-
     // New Border Properties
     property BorderColor: TAlphaColor read FBorderColor write SetBorderColor default $FF1E3A8A;
     property BorderWidth: Single read FBorderWidth write SetBorderWidth;
     property RoundCorners: Single read FRoundCorners write SetRoundCorners;
     property ShowBorder: Boolean read FShowBorder write SetShowBorder default True;
-
     property OnClick: TNotifyEvent read FOnClick write FOnClick;
     property OnMouseDown: TMouseEvent read FOnMouseDown write FOnMouseDown;
     property OnMouseUp: TMouseEvent read FOnMouseUp write FOnMouseUp;
     property OnMouseEnter: TNotifyEvent read FOnMouseEnter write FOnMouseEnter;
     property OnMouseLeave: TNotifyEvent read FOnMouseLeave write FOnMouseLeave;
-
     property Align;
     property Position;
     property Width;
@@ -148,18 +123,13 @@ type
     property Visible;
     property HitTest;
   end;
-
 procedure Register;
-
 implementation
-
 procedure Register;
 begin
   RegisterComponents('Skia', [TSkiaButton]);
 end;
-
 { TSkiaButton }
-
 constructor TSkiaButton.Create(AOwner: TComponent);
 begin
   inherited;
@@ -167,43 +137,36 @@ begin
   FTimer.Interval := 16;
   FTimer.OnTimer := OnTimer;
   FTimer.Enabled := False;
-
   FText := 'Skia Button';
   FButtonStyle := bsFlat;
   FHoverEffect := heGlow;
   FShowTransition := ttFade;
   FHideTransition := ttImplode;
-
   // Default Colors
   FColor := $FF3B82F6;       // Blue
   FHoverColor := $FF4B8BF6;  // Lighter Blue
   FPressedColor := $FF2A6ACB; // Darker Blue
   FTextColor := TAlphaColors.White;
-
   // Default Border Settings
   FBorderColor := $FF1E3A8A;  // Dark Blue Border
   FBorderWidth := 2.0;
   FRoundCorners := 8.0;
   FShowBorder := True;
-
   FAnimationState := asIdle;
   FAnimProgress := 1.0;
   FIsHovered := False;
   FIsPressed := False;
   FTime := 0;
   FMousePos := TPointF.Create(0,0);
-
   HitTest := True;
   SetBounds(0, 0, 140, 50);
   Redraw;
 end;
-
 destructor TSkiaButton.Destroy;
 begin
   FTimer.Free;
   inherited;
 end;
-
 procedure TSkiaButton.SetText(const Value: string);
 begin
   if FText <> Value then
@@ -212,7 +175,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetButtonStyle(const Value: TButtonStyle);
 begin
   if FButtonStyle <> Value then
@@ -221,7 +183,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetColor(const Value: TAlphaColor);
 begin
   if FColor <> Value then
@@ -230,7 +191,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetHoverColor(const Value: TAlphaColor);
 begin
   if FHoverColor <> Value then
@@ -239,7 +199,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetPressedColor(const Value: TAlphaColor);
 begin
   if FPressedColor <> Value then
@@ -248,7 +207,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetTextColor(const Value: TAlphaColor);
 begin
   if FTextColor <> Value then
@@ -257,7 +215,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetBorderColor(const Value: TAlphaColor);
 begin
   if FBorderColor <> Value then
@@ -266,7 +223,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetBorderWidth(const Value: Single);
 begin
   if FBorderWidth <> Value then
@@ -275,7 +231,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetRoundCorners(const Value: Single);
 begin
   if FRoundCorners <> Value then
@@ -284,7 +239,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetShowBorder(const Value: Boolean);
 begin
   if FShowBorder <> Value then
@@ -293,7 +247,6 @@ begin
     InvalidateBuffer;
   end;
 end;
-
 procedure TSkiaButton.SetHovered(const Value: Boolean);
 begin
   if FIsHovered <> Value then
@@ -311,7 +264,6 @@ begin
     UpdateState;
   end;
 end;
-
 procedure TSkiaButton.SetPressed(const Value: Boolean);
 begin
   if FIsPressed <> Value then
@@ -321,33 +273,27 @@ begin
     UpdateState;
   end;
 end;
-
 procedure TSkiaButton.UpdateState;
 var
   IsIdle: Boolean;
 begin
   IsIdle := (FAnimationState = asIdle) and not FIsHovered and not FIsPressed;
-
   if IsIdle then
     FTimer.Enabled := False
   else
     FTimer.Enabled := True;
-
   InvalidateRect(LocalRect);
 end;
-
 procedure TSkiaButton.InvalidateBuffer;
 begin
   FBackBuffer := nil;
   Redraw;
 end;
-
 procedure TSkiaButton.OnTimer(Sender: TObject);
 const
   Delta = 0.016;
 begin
   FTime := FTime + Delta;
-
   if FAnimationState <> asIdle then
   begin
     FAnimProgress := FAnimProgress + Delta;
@@ -365,10 +311,8 @@ begin
       UpdateState;
     end;
   end;
-
   Redraw;
 end;
-
 procedure TSkiaButton.ShowAnimated;
 begin
   Visible := True;
@@ -378,7 +322,6 @@ begin
   UpdateState;
   Repaint;
 end;
-
 procedure TSkiaButton.HideAnimated;
 begin
   if FAnimationState <> asHiding then
@@ -389,7 +332,6 @@ begin
     UpdateState;
   end;
 end;
-
 procedure TSkiaButton.MouseMove(Shift: TShiftState; X, Y: Single);
 begin
   inherited;
@@ -399,13 +341,11 @@ begin
   else
     SetHovered(False);
 end;
-
 procedure TSkiaButton.DoMouseLeave;
 begin
   inherited;
   SetHovered(False);
 end;
-
 procedure TSkiaButton.MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Single);
 begin
   inherited;
@@ -415,7 +355,6 @@ begin
     if Assigned(FOnMouseDown) then FOnMouseDown(Self, Button, Shift, X, Y);
   end;
 end;
-
 procedure TSkiaButton.MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Single);
 begin
   inherited;
@@ -427,11 +366,9 @@ begin
       if Assigned(FOnClick) then FOnClick(Self);
   end;
 end;
-
 procedure TSkiaButton.Draw(const ACanvas: ISkCanvas; const ADest: TRectF; const AOpacity: Single);
 begin
   inherited Draw(ACanvas, ADest, AOpacity);
-
   if not Assigned(FBackBuffer) or
      (FBackBuffer.Width <> Round(Width)) or
      (FBackBuffer.Height <> Round(Height)) or
@@ -441,13 +378,11 @@ begin
   begin
     RenderToBuffer(Round(Width), Round(Height));
   end;
-
   if Assigned(FBackBuffer) then
     ACanvas.DrawImage(FBackBuffer, 0, 0, TSkSamplingOptions.High)
   else
     ACanvas.Clear(TAlphaColors.Null);
 end;
-
 procedure TSkiaButton.RenderToBuffer(const AWidth, AHeight: Integer);
 var
   Surface: ISkSurface;
@@ -465,20 +400,18 @@ var
   PathBuilder: ISkPathBuilder;
   ClipPath: ISkPath;
   RoundRect: ISkRoundRect;
+  CycleSeedInt: Integer;
+  BrightnessMul: Double;
 begin
   if (AWidth <= 0) or (AHeight <= 0) then Exit;
-
   Surface := TSkSurface.MakeRaster(AWidth, AHeight);
   if not Assigned(Surface) then Exit;
-
   Canvas := Surface.Canvas;
   DestRect := RectF(0, 0, AWidth, AHeight);
   Canvas.Clear(TAlphaColors.Null);
   SaveCount := Canvas.Save;
-
   Opacity := 1.0;
   Scale := 1.0;
-
   // Determine which transition to use
   if FAnimationState = asHiding then
     TransType := FHideTransition
@@ -486,17 +419,14 @@ begin
     TransType := FShowTransition
   else
     TransType := ttFade;
-
   // --- 1. Calculate Hover/Pressed Scales ---
   if FIsPressed then
     Scale := Scale * 0.95
   else if FIsHovered and (FHoverEffect = heScaleUp) then
     Scale := Scale * 1.05;
-
   // ========================================================================
   // FIX: We MUST apply the Hover Animations (Liquid/Tilt) BEFORE the Clip
   // ========================================================================
-
   // --- 2. Apply Hover Effects (Liquid & Tilt) ---
   if not (FAnimationState in [asShowing, asHiding]) then
   begin
@@ -505,13 +435,11 @@ begin
       Pulse := Sin(FTime * 5) * 0.02;
       Scale := Scale + Pulse;
     end;
-
     if (Scale <> 1.0) or (FIsHovered and (FHoverEffect = heTilt)) then
     begin
       Center := PointF(AWidth / 2, AHeight / 2);
       Canvas.Translate(Center.X, Center.Y);
       Canvas.Scale(Scale, Scale);
-
       if FIsHovered and (FHoverEffect = heTilt) then
       begin
         NormX := ((FMousePos.X / AWidth) - 0.5) * 2;
@@ -519,34 +447,28 @@ begin
         Canvas.Scale(1.0 - (NormX * 0.1), 1.0 - (NormY * 0.1));
         Canvas.Translate(NormX * 5, NormY * 5);
       end;
-
       Canvas.Translate(-Center.X, -Center.Y);
     end;
   end;
-
   // --- 3. Apply Global Transition Transforms ---
   if (FAnimationState = asShowing) or (FAnimationState = asHiding) then
   begin
     Canvas.RestoreToCount(SaveCount);
     SaveCount := Canvas.Save;
-
     case TransType of
       ttFade:
         Opacity := IfThen(FAnimationState = asHiding, 1.0 - FAnimProgress, FAnimProgress);
-
       ttImplode:
         begin
           if FAnimationState = asHiding then
             Scale := 1.0 - FAnimProgress
           else
             Scale := FAnimProgress;
-
           Center := PointF(AWidth / 2, AHeight / 2);
           Canvas.Translate(Center.X, Center.Y);
           Canvas.Scale(Scale, Scale);
           Canvas.Translate(-Center.X, -Center.Y);
         end;
-
       tsExplode:
         begin
           if FAnimationState = asHiding then
@@ -558,7 +480,6 @@ begin
           begin
             Scale := 1.5 - (FAnimProgress * 0.5);
           end;
-
           Center := PointF(AWidth / 2, AHeight / 2);
           Canvas.Translate(Center.X, Center.Y);
           Canvas.Scale(Scale, Scale);
@@ -566,7 +487,6 @@ begin
         end;
     end;
   end;
-
   // --- 4. Clip to Rounded Rectangle ---
   if FRoundCorners > 0 then
   begin
@@ -577,21 +497,18 @@ begin
     ClipPath := PathBuilder.Detach;
     Canvas.ClipPath(ClipPath);
   end;
-
   if Opacity < 1.0 then
   begin
     Paint := TSkPaint.Create;
     Paint.AlphaF := Opacity;
     Canvas.SaveLayer(Paint);
   end;
-
   // --- 5. Draw Styles ---
   case FButtonStyle of
     bsFlat:    DrawFlatStyle(Canvas, DestRect, 1.0);
     bsNeon:    DrawNeonStyle(Canvas, DestRect, 1.0);
     bsRetro:   DrawRetroStyle(Canvas, DestRect, 1.0);
   end;
-
   // --- 6. Draw Border (If Enabled) ---
   if FShowBorder and (FBorderWidth > 0) then
   begin
@@ -602,26 +519,21 @@ begin
     Paint.AntiAlias := True;
     Canvas.DrawRoundRect(DestRect, FRoundCorners, FRoundCorners, Paint);
   end;
-
   // --- 7. Draw Overlays ---
-if FIsHovered and (FHoverEffect = heRipple) then
-begin
-  Pulse := (FTime * 0.5) - Floor(FTime * 0.5);
+  if FIsHovered and (FHoverEffect = heRipple) then
+  begin
+    Pulse := (FTime * 0.5) - Floor(FTime * 0.5);
+    CycleSeedInt := Trunc(FTime * 0.5);
+    BrightnessMul := 0.75 + 0.45 * ((CycleSeedInt mod 7) / 7.0);
 
-  // Simple per-cycle variation (changes when entering new ripple)
-  var CycleSeed := Floor(FTime * 0.5);
-  var BrightnessMul := 0.75 + 0.45 * (CycleSeed mod 7 / 7.0); // 0.75–1.2 range
-
-  Paint := TSkPaint.Create;
-  Paint.Style := TSkPaintStyle.Fill;
-  Paint.Color := $FFFFFF;
-  Paint.AlphaF := 0.48 * BrightnessMul * (1.0 - Pulse);
-
-  Canvas.DrawCircle(PointF(AWidth/2, AHeight/2),
-                    Pulse * (AWidth * 1.15),
-                    Paint);
-end;
-
+    Paint := TSkPaint.Create;
+    Paint.Style := TSkPaintStyle.Fill;
+    Paint.Color := $FFFFFF;
+    Paint.AlphaF := 0.48 * BrightnessMul * (1.0 - Pulse);
+    Canvas.DrawCircle(PointF(AWidth/2, AHeight/2),
+                      Pulse * (AWidth * 1.15),
+                      Paint);
+  end;
   if FIsHovered and (FHoverEffect = heGlow) then
   begin
      Paint := TSkPaint.Create;
@@ -632,7 +544,6 @@ end;
      Paint.MaskFilter := TSkMaskFilter.MakeBlur(TSkBlurStyle.Solid, 4);
      Canvas.DrawRoundRect(DestRect, FRoundCorners, FRoundCorners, Paint);
   end;
-
   if FIsHovered and (FHoverEffect = heSpot) then
   begin
     Paint := TSkPaint.Create;
@@ -640,7 +551,6 @@ end;
     Paint.Color := $AA000000;
     Canvas.DrawCircle(FMousePos, 40, Paint);
   end;
-
   // Text
   Paint := TSkPaint.Create;
   Paint.Style := TSkPaintStyle.Fill;
@@ -649,14 +559,11 @@ end;
   Typeface := TSkTypeface.MakeDefault;
   Font := TSkFont.Create(Typeface, 20);
   DrawCenteredText(Canvas, FText, DestRect, Font, Paint);
-
   if Opacity < 1.0 then
     Canvas.Restore;
-
   Canvas.RestoreToCount(SaveCount);
   FBackBuffer := Surface.MakeImageSnapshot;
 end;
-
 procedure TSkiaButton.DrawCenteredText(const ACanvas: ISkCanvas; const AText: string; const ADest: TRectF; const AFont: ISkFont; const APaint: ISkPaint);
 var
   TextBlob: ISkTextBlob;
@@ -673,7 +580,6 @@ begin
   TextPos.Y := (ADest.Height / 2) + (EstimatedHeight / 3);
   ACanvas.DrawTextBlob(TextBlob, TextPos.X, TextPos.Y, APaint);
 end;
-
 procedure TSkiaButton.DrawFlatStyle(const ACanvas: ISkCanvas; const ARect: TRectF; const AOpacity: Single);
 var
   Paint: ISkPaint;
@@ -688,21 +594,16 @@ begin
     Paint.Color := FHoverColor
   else
     Paint.Color := FColor;
-
   Paint.AntiAlias := True;
-
   R := ARect;
-
   // For Flat style, we generally want to respect the border width by shrinking the background
   // so the border (if any) doesn't overlap it weirdly, OR we just fill the whole thing.
   // Given the ClipPath in RenderToBuffer handles the shape, we just fill the rect.
   // However, if BorderWidth is large, we might want to inset.
   // For simplicity, we fill the whole rect, border draws on top.
-
   if FIsHovered and (FHoverEffect = heLiquid) then
   begin
     ACanvas.DrawRoundRect(R, FRoundCorners, FRoundCorners, Paint);
-
     Paint.Style := TSkPaintStyle.Stroke;
     Paint.StrokeWidth := 2;
     Paint.Color := TAlphacolors.White;
@@ -715,7 +616,6 @@ begin
     ACanvas.DrawRoundRect(R, FRoundCorners, FRoundCorners, Paint);
   end;
 end;
-
 procedure TSkiaButton.DrawNeonStyle(const ACanvas: ISkCanvas; const ARect: TRectF; const AOpacity: Single);
 var
   Paint: ISkPaint;
@@ -726,7 +626,6 @@ begin
   Paint.Color := $FF101010; // Dark background for Neon
   R := ARect;
   ACanvas.DrawRoundRect(R, FRoundCorners, FRoundCorners, Paint);
-
   Paint := TSkPaint.Create;
   Paint.Style := TSkPaintStyle.Stroke;
   Paint.StrokeWidth := 4;
@@ -735,7 +634,6 @@ begin
   Paint.MaskFilter := TSkMaskFilter.MakeBlur(TSkBlurStyle.Normal, 5);
   ACanvas.DrawRoundRect(R, FRoundCorners, FRoundCorners, Paint);
 end;
-
 procedure TSkiaButton.DrawRetroStyle(const ACanvas: ISkCanvas; const ARect: TRectF; const AOpacity: Single);
 var
   Paint: ISkPaint;
@@ -747,20 +645,15 @@ begin
   Paint.Color := FColor;
   R := ARect;
   ACanvas.DrawRect(R, Paint);
-
   Paint.Style := TSkPaintStyle.Stroke;
   Paint.StrokeWidth := 4;
-
   Lighter := FHoverColor;
   Darker := FPressedColor;
-
   Paint.Color := Lighter;
   ACanvas.DrawLine(R.Left, R.Top, R.Right, R.Top, Paint);
   ACanvas.DrawLine(R.Left, R.Top, R.Left, R.Bottom, Paint);
-
   Paint.Color := Darker;
   ACanvas.DrawLine(R.Right, R.Top, R.Right, R.Bottom, Paint);
   ACanvas.DrawLine(R.Left, R.Bottom, R.Right, R.Bottom, Paint);
 end;
-
 end.
